5d01e25a8861c4de65522e1102561171f1636a42
vincent guipouy <vincent.guipouy@accenture.com>
2018-04-27 17:29:09 +0400

    Logout link and login form completed

--- final/src/main/java/com/accenture/accademy/spring/security/config/WebSecurityConfig.java
+++ final/src/main/java/com/accenture/accademy/spring/security/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.accenture.accademy.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +15,23 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll();
+    }
+
     /**
      * In memory user details service.
      * This user details service is used by DaoAuthenticationProvider to get user details.
